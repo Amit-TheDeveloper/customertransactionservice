9af6c54f35037b1c2f1f87b1d20e945d8423b99f
Amit Chandra <amchandra@paypal.com>
2022-05-25 23:18:02 +0530

    initial commit

---
+++ src/main/java/com/enterprise/payments/customertransactionservice/CustomertransactionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.payments.customertransactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomertransactionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomertransactionserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enterprise/payments/customertransactionservice/CustomertransactionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.payments.customertransactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomertransactionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
